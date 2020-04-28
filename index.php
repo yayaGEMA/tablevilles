b532f40e93151894593021e869a1bf4999fc53d7
Marceau.GERARD <marceaugerard1610@gmail.com>
2020-04-28 09:47:07 +0200

    index complété, avec exemple de tableau

--- index.php
+++ index.php
@@ -6,13 +6,53 @@
     <title>Villes</title>
 </head>
 <style>
-
+    .titles, form{
+        text-align: center;
+    }
+    .titles p{
+        font-size: 2rem;
+    }
+    table{
+        margin: auto;
+        margin-top: 30px;
+        border: 2px solid black;
+        width: 50%;
+        padding: 1rem;
+        text-align: center;
+    }
+    table tr td{
+        padding: 1rem;
+    }
 </style>
 <body>
-    <h1>Récupérer les infos d'une ville à partir de l'API<br>
-    <a href="https://api.gouv.fr/api/api-geo.html">https://api.gouv.fr/api/api-geo.html</a></h1>
+    <div class="titles">
+        <h1>Récupérer les infos d'une ville à partir de l'api :<br>
+        <a href="https://api.gouv.fr/api/api-geo.html">https://api.gouv.fr/api/api-geo.html</a></h1>
+        <p>URL de base :<br>https://geo.api.gouv.fr/communes/?nom=VILLE</p>
+    </div>
+    <form action="" method="GET">
+        <input type="text" placeholder="Ville recherchée">
+        <input type="submit">
+    </form>
 
-
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Codes postaux</th>
+                <th>Population</th>
+                <th>Département</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Chalon-sur-Saône</td>
+                <td>71100</td>
+                <td>45446</td>
+                <td>71</td>
+            </tr>
+        </tbody>
+    </table>
 
     <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
     <script src="js/script.js"></script>
