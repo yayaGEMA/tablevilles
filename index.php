63e0a5711edd554bf213c8db5127fd3caae98fac
Marceau.GERARD <marceaugerard1610@gmail.com>
2020-04-28 09:28:41 +0200

    Créé les fichiers

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Villes</title>
+</head>
+<style>
+
+</style>
+<body>
+    <h1>Récupérer les infos d'une ville à partir de l'API<br>
+    <a href="https://api.gouv.fr/api/api-geo.html">https://api.gouv.fr/api/api-geo.html</a></h1>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
